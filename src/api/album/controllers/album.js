49347bf096ae1c7e0f0a117c6e00fbba77bbcf90
Dev-LucasC <lucaconceicao@hotmail.com>
2023-05-09 13:59:08 -0300

    Funcionando

--- src/api/album/controllers/album.js
+++ src/api/album/controllers/album.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * teste controller
+ * album controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::teste.teste');
+module.exports = createCoreController('api::album.album');
